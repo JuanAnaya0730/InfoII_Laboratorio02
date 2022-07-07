bdf4c0f3bedbf7a67cf280a7fb0f0108c8ea4385
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-07-07 01:22:07 -0500

    Problema terminado

--- Lab2_Problema06_AnayaJuan/main.cpp
+++ Lab2_Problema06_AnayaJuan/main.cpp
@@ -1,9 +1,42 @@
+/*
+                          INFORMATICA II
+Grupo 05
+Juan Sebastian Anaya Regino
+
+Problema 6. Escriba un programa que reciba una cadena de caracteres y cambie las letras minúsculas
+por mayúsculas, los demás caracteres no deben ser alterados.
+Ejemplo: se recibe Man-zana debe mostrar MAN-ZANA.
+Nota: la salida del programa debe ser: Original: Man-zana. En mayuscula: MAN-ZANA.
+
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char charString[50]; // Array que almacenara la cadena ingresada
+
+    /* Se imprime en pantalla la funcion del programa. */
+    cout << "Este programa recibe una cadena de caracteres y cambia las letras minusculas por mayusculas.\n"
+            "Nota: Los caracteres diferentes a letras minusculas no seran modificados.\n" << endl;
+
+    /* Se le pide al usuario que ingrese una cadena de caracteres */
+    cout << "Ingrese la cadena de caracteres: ";
+    cin.getline(charString, 50);
+
+    /* Se imprime la cadena original */
+    cout << "\nOriginal: " << charString << endl;
+
+    for(int i=0; charString[i] != '\0'; ++i){
+        if(charString[i] >= 97 && charString[i] <= 122){
+            charString[i] -= 32;
+        }
+    }
+
+    /* Se imprime la cadena ingresada en mayusculas */
+    cout << "Mayuscula: " << charString << endl;
+
     return 0;
 }
--- Lab2_Problema08_AnayaJuan/main.cpp
+++ Lab2_Problema08_AnayaJuan/main.cpp
@@ -1,9 +1,51 @@
+/*
+                          INFORMATICA II
+Grupo 05
+Juan Sebastian Anaya Regino
+
+Problema 8. Escriba un programa que reciba una cadena de caracteres y separe los números del resto de caracteres,
+generando una cadena que no tiene números y otra con los números que había en la cadena original.
+Ejemplo: Si se recibe abc54rst el programa debe imprimir las cadenas: abcrst y 54.
+Nota: la salida del programa debe ser:
+Original: abc54rst.
+Texto: abcrst. Numero: 54
+
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char charString[50]="", textString[50]="", numericString[50]="";
+
+    /* Se imprime en pantalla la funcion del programa. */
+    cout << "Este programa recibe una cadena de caracteres y separa los caracteres "
+            "numericos de los no numericos.\n" << endl;
+
+    /* Se le pide al usuario que ingrese la cadena de caracteres */
+    cout << "Ingrese la cadena de caracteres: ";
+    cin.getline(charString, 50);
+
+    /* Aqui se separan los caracteres numericos de los no numericos */
+    for(int i=0, texIndex=0, numIndex=0; charString[i] != '\0'; ++i){
+
+        /* Se verifica si el caracter de determinada posicion es numerico */
+        if(charString[i] >= 48 && charString[i] <= 57){
+            numericString[numIndex] = charString[i];
+            numIndex++;
+        }else{
+            textString[texIndex] = charString[i];
+            texIndex++;
+        }
+    }
+
+    /* Se imprime la cadena original */
+    cout << "\nOriginal: " << charString  << endl;
+
+    /* Se imprimen por separado los caracteres numericos y no numericos de la cadena original */
+    cout << "Texto: " << textString << " Numero: " << numericString << endl;
+
     return 0;
 }
